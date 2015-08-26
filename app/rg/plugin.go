252297ab93c09ab4e070d6bb6ef940ad24093c5b
Levi Lovelock <lovelock.levi@gmail.com>
2015-08-26 00:31:13 -0500

    rg: Added skeleton for basic plugin and note for complete architecture

--- app/rg/plugin.go
+++ app/rg/plugin.go
@@ -5,10 +5,11 @@ import (
 
 	graphite "github.com/marpaia/graphite-golang"
 	"github.com/revel/revel"
+	"github.com/robfig/cron"
 )
 
 var (
-	g    graphite.Graphite
+	g    *graphite.Graphite
 	host string
 	port int
 )
@@ -18,20 +19,43 @@ func init() {
 		host, hostFound := revel.Config.String("revel-graphite.host")
 
 		if !hostFound {
-			fmt.Println("Expecting revel-graphite.host to be set in config, but it wasn't!")
+			revel.ERROR.Println("Expecting revel-graphite.host to be set in config, but it wasn't!")
 		}
 
 		port, portFound := revel.Config.Int("revel-graphite.port")
 		if !portFound {
-			fmt.Println("Expecting revel-graphite.host to be set in config, but it wasn't!")
+			revel.ERROR.Println("Expecting revel-graphite.host to be set in config, but it wasn't!")
 		}
 
-		g, err := graphite.NewGraphite(host, port)
+		var err error
+		g, err = graphite.NewGraphite(host, port)
 		if err != nil {
-			fmt.Printf("Failed to connect to graphite server with host: '%s' and port: '%d'.\n", host, port)
+			revel.ERROR.Printf("Failed to connect to graphite server with host: '%s' and port: '%d'.\n", host, port)
 			return
+		} else {
+			revel.INFO.Printf("Connected to graphite server '%s:%d' succesfully.\n", host, port)
 		}
 
-		g.SimpleSend("test.stat", "20")
+		c := cron.New()
+		c.AddFunc("@every 10s", sessionsMetric)
+		c.Start()
 	})
+}
+
+func sessionsMetric() {
+	/* TODO: Fix it so it sends an actual metric
+	 * One way to do this would be to create a filter for the plugin which we tell teh users to
+	 * add at the end (or at least after sessions for this exact case), then that filter will
+	 * check for a session and increment a global counter. Probably the counter will be reset every 10s or
+	 * 30s or 60s and then we actually publish the metric to graphite then and there with the past minutes
+	 * data or whatever. We would probably need to have a session map to not double count the same sessions
+	 * too. If the user wasn't using the default session filter we could make session (and other standard
+	 * metrics) opt-out via module-specific-configuration. We could probably do most web-metrics via this
+	 * filter approach.
+	 */
+
+	err := g.SimpleSend(fmt.Sprintf("revel.%s.sessions", revel.AppName), "0")
+	if err != nil {
+		revel.ERROR.Printf("Cannot send session stats to graphite server '%s:%d'.\n", host, port)
+	}
 }
